fd4a635ab98231f93abc15c039bdeef33631019f
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-07-25 18:53:40 +0300

    db

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
-<?php
+<?php  
+/*
+EjAPj6bdC&iZVNG
 $host = 'localhost';
 $dbname = 'myForm_db';
 $username = 'root';
@@ -9,4 +11,20 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?> */
+
+
+$host = "db4free.net";
+$port = 3306;
+$username = "yasmin";
+$password = "EjAPj6bdC&iZVNG"; 
+$database = "myform_db";
+
+$conn = new mysqli($host, $username, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
+
+
